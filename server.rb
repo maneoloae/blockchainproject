ffbd7e490ace6b4073ded110525e1d9f53bbb70c
Morgan Lim <maneoloae@gmail.com>
2018-04-24 21:24:26 +0900

    sever주소명추가
    server주소명 및 내용추가

--- server.rb
+++ server.rb
@@ -1,7 +1,9 @@
 # encoding: UTF-8 ##한글지원을 위한 코드
 
-require 'sinatra'
+require 'sinatra' #webserver 구동을 위한 서버 참고 ; http://sinatrarb.com #
 
-get '/' do 
-	"block"
+get '/join' do ##주소가 들어가는 곳	
+	'회원가입 페이지입니'
+
+
 end
