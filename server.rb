f58f6d587c2af25beb793cf5df38238f30a7b52f
Morgan Lim <maneoloae@gmail.com>
2018-04-24 21:28:30 +0900

    blockchain_tag추가
    blockchain_tag추가

--- server.rb
+++ server.rb
@@ -2,8 +2,13 @@
 
 require 'sinatra' #webserver 구동을 위한 서버 참고 ; http://sinatrarb.com #
 
-get '/join' do ##주소가 들어가는 곳	
+get '/mining' do ##주소가 들어가는 곳	
 	'회원가입 페이지입니'
 
+end
+
+
+get '/make_a_transaction' do	
+	'로그아웃 하셨습니다.'
 
 end
