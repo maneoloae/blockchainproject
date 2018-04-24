4a95f78efaca06ce2333ae12831b85ee3a751a64
Morgan Lim <maneoloae@gmail.com>
2018-04-24 21:31:58 +0900

    서버부팅자동화&html추가
    서버부팅자동화&html_tag추가

--- server.rb
+++ server.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8 ##한글지원을 위한 코드
 
 require 'sinatra' #webserver 구동을 위한 서버 참고 ; http://sinatrarb.com #
-
+require 'sinatra/reloader' ## server on/off 반복 최소화를 하기위하ㅇ
 get '/mining' do ##주소가 들어가는 곳	
-	'회원가입 페이지입니'
+	"<h1 style='text-align:center'>채굴로 달리자으아아아.</h1>" ##html 및 CSS를 넣어서 꾸미면 됨
 
 end
 
