ba3f8265be3e547a7e47115a59098ed50501f7de
Morgan Lim <maneoloae@gmail.com>
2018-04-24 22:32:01 +0900

    class변수강의
    class 변수 및 코드 간결화 강의

--- class_defexplanation.rb
+++ class_defexplanation.rb
@@ -1,7 +1,17 @@
 
 class Dog #클래스는 소문자, 무조건 네이밍 단어 첫 번째는 알파벳 대문자
 
+	def initialize #자동적으로 한바퀴돌고 시작함
+		@hungry = true ##@가 붙었다는 뜻은 이 클래스 내에서 아무 곳에서 쓸 수 있는 변수
+		@weight = 0.5
+		@age = 1
+	end
+	def my_weight
+		puts "현재 몸무게 " + @weight.to_s ##a문자와 숫자값을 더할 때, 변환을 해줘야 함
+	end
+
 	def eat
+		@weight = @weight + 0.5
 		puts "먹는다"
 	end
 
@@ -14,9 +24,14 @@ class Dog #클래스는 소문자, 무조건 네이밍 단어 첫 번째는 알�
 	end
 end
 
-
 aa = Dog.new ##Dog를 대입
-aa.run ##강아지가 aa란 이름으로 활동할 것 
+bb = Dog.new ##다른 강아지가 태어나 각기 성장함
+aa.my_weight##강아지가 .뒤에 붙는 변수로 활동할 것 
+aa.eat
+aa.my_weight
+#전체 시스템 상태 유지를 위해서 도입하는 개념, 일종의 성질
+
+#블록체인으로 치면, 노드가 계속 추가되는 것을 의미
 
 
 
