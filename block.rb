8e8013cc0a2c8385bb5d691f9276e9eb332f0b15
Morgan Lim <maneoloae@gmail.com>
2018-04-27 20:37:56 +0900

    test문제대입
    마이닝을 위한 test문제대입

---
+++ block.rb
@@ -0,0 +1,12 @@
+class Blockchain ###설계도청사진으로 logic만 들고있
+
+	def mining ###0부터 10사이의 아무 숫자나 찍었을 때, 0이면 마이닝 성공####
+
+	begin
+		nonce = rand(10) #1부터 10사이에 랜덤으로 뽑겠다
+
+	end while nonce !=0 #0이 아니면 계속 돌리겠
+
+
+	end
+end
