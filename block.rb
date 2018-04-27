27bae9822a68d9eb7aedb7780f9c5cce9a67d229
Morgan Lim <maneoloae@gmail.com>
2018-04-27 20:46:04 +0900

    nonce loop logic 추가
    nonce loop logic 추가

--- block.rb
+++ block.rb
@@ -2,11 +2,11 @@ class Blockchain ###설계도청사진으로 logic만 들고있
 
 	def mining ###0부터 10사이의 아무 숫자나 찍었을 때, 0이면 마이닝 성공####
 
-	begin
-		nonce = rand(10) #1부터 10사이에 랜덤으로 뽑겠다
+	begin #begin에서 시작해라#
+		nonce = rand(10) #1부터 10사이에 랜덤으로 뽑아서 nonce에 대입해라#
 
-	end while nonce !=0 #0이 아니면 계속 돌리겠
-
+	end while nonce !=0 #0이 아니면 계속 돌린다. !는 붙여서#
+	nonce		#함수가 끝나면서 제일 마지막에 나오는 수(nonce값직전가 대체함#)
 
 	end
 end
